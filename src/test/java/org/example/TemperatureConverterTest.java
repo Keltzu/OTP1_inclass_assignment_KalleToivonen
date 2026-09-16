0c1adf75d0ed0b1471c29b164a30dc2b900b31cc
Keltzu <kalleto@metropolia.fi>
2026-09-16 11:37:36 +0300

    Kelvintocelsius funktio ja päivitetty coverage koodi raportti sekä main luokka poistettu

--- src/test/java/org/example/TemperatureConverterTest.java
+++ src/test/java/org/example/TemperatureConverterTest.java
@@ -29,4 +29,11 @@ class TemperatureConverterTest {
         assertFalse(converter.isExtremeTemperature(50));
         assertFalse(converter.isExtremeTemperature(25));
     }
+
+    @Test
+    void testKelvinToCelsius() {
+        assertEquals(26.85, converter.kelvinToCelsius(300), 0.01);
+        assertEquals(0, converter.kelvinToCelsius(273.15), 0.01);
+        assertEquals(-273.15, converter.kelvinToCelsius(0), 0.01);
+    }
 }
